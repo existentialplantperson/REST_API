dfb2e95c35fb82fc63c5de4d066ce393f97bed62
Angela Spencer <livesimplyspencer@gmail.com>
2022-02-20 19:25:15 -0600

    modify app

--- flask-app.py
+++ flask-app.py
@@ -14,24 +14,24 @@ app = Flask(__name__)
 def echo_hello():
     return "Hello World!"
 
-@app.route("/gdp")
-def gdp():
-    json_url = os.path.join(app.static_folder,"","us_gdp.json")
-    data_json = json.load(open(json_url))
+#@app.route("/gdp")
+#def gdp():
+#    json_url = os.path.join(app.static_folder,"","us_gdp.json")
+#     data_json = json.load(open(json_url))
 
-    return render_template('index.html',data=data_json)
+#     return render_template('index.html',data=data_json)
 
-@app.route("/gdp/<year>")
-def gdp_year(year):
-    json_url = os.path.join(app.static_folder,"","us_gdp.json")
-    data_json = json.load(open(json_url))
+# @app.route("/gdp/<year>")
+# def gdp_year(year):
+#     json_url = os.path.join(app.static_folder,"","us_gdp.json")
+#     data_json = json.load(open(json_url))
 
-    data = data_json[1]
-    #print(data)
-    year = request.view_args['year']
+#     data = data_json[1]
+#     #print(data)
+#     year = request.view_args['year']
 
-    output_data = [x for x in data if x['date']==year]
-    return render_template('index.html',data=output_data)
+#     output_data = [x for x in data if x['date']==year]
+#     return render_template('index.html',data=output_data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+     app.run(debug=True)
